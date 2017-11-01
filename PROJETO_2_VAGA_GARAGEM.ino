3876c22fa428c6998499f3c35a23e91a59b9f68a
ODIRLEI SABELLA DE ASSIS <32184284831@jpmorgan.sp>
2017-11-01 21:45:01 -0200

    primeiro commit

---
+++ PROJETO_2_VAGA_GARAGEM.ino
@@ -0,0 +1,92 @@
+#include <LiquidCrystal.h>
+
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 8;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+#include <SPI.h>
+#include <UIPEthernet.h>
+#include <utility/logging.h>
+#include <PubSubClient.h>
+
+byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xF1, 0x68};
+
+void callback(char *topic, byte * payload, unsigned int length);
+
+EthernetClient ethClient;
+PubSubClient client("m14.cloudmqtt.com", 14064, callback, ethClient);
+
+long lastReconnectAttempt = 0;
+
+boolean reconnect() {
+  Serial.println("reconectando...");
+  if (client.connect("arduino", "arduino", "arduino")) {
+    Serial.println("conectado");
+    client.publish("teste", "hello world");
+    client.subscribe("teste");
+  }
+  Serial.println("Conectado MQTT");
+  return client.connected();
+}
+void callback(char* topic, byte * payload, unsigned int length) {
+  int msg = atoi(payload);
+
+  Serial.println(msg);
+  Serial.println(topic);
+
+  if (msg == 1) {
+    Serial.println("entrou if");
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+    lcd.setCursor(0, 1);
+    lcd.print(0);
+    delay(1000);
+  }
+  else {
+    Serial.println("else");
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+    lcd.setCursor(0, 1);
+    lcd.print(1);
+    delay(1000);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("iniciando...");
+  Ethernet.begin(mac);
+  delay(1500);
+  lastReconnectAttempt = 0;
+
+if (client.connect("arduino", "arduino", "arduino"))
+  {
+  // Conecta no topic para receber mensagens
+  client.subscribe("teste");
+  
+  Serial.println("Conectado MQTT");
+  delay(50);
+  }
+
+  lcd.begin(16, 2);
+  lcd.print("Vagas Livres:");
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    long now = millis();
+    if ((now - lastReconnectAttempt) > 5000) {
+      Serial.println("reconectando...");
+      lastReconnectAttempt = now;
+
+      if (reconnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+  } else {
+    client.loop();
+  }
+}
+
+
