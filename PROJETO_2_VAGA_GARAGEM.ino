f3c3d4d7c1149f10fa85d37769aade89c6615340
ODIRLEI SABELLA DE ASSIS <32184284831@jpmorgan.sp>
2017-11-07 19:50:02 -0200

    inclusao do led

--- PROJETO_2_VAGA_GARAGEM.ino
+++ PROJETO_2_VAGA_GARAGEM.ino
@@ -1,5 +1,6 @@
 #include <LiquidCrystal.h>
 
+const int ledVerde = 9;
 const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -21,16 +22,23 @@ long lastReconnectAttempt = 0;
 boolean reconnect() {
   Serial.println("reconectando...");
   if (client.connect("arduino", "arduino", "arduino")) {
+    digitalWrite(ledVerde, HIGH);
     Serial.println("conectado");
     client.publish("teste", "hello world");
-    client.subscribe("teste");
+    client.subscribe("teste#");
   }
   Serial.println("Conectado MQTT");
   return client.connected();
 }
 void callback(char* topic, byte * payload, unsigned int length) {
   int msg = atoi(payload);
-
+  digitalWrite(ledVerde, LOW);
+  delay(100);
+  digitalWrite(ledVerde, HIGH);
+  delay(100);
+  digitalWrite(ledVerde, LOW);
+  delay(100);
+  digitalWrite(ledVerde, HIGH);
   Serial.println(msg);
   Serial.println(topic);
 
@@ -39,7 +47,7 @@ void callback(char* topic, byte * payload, unsigned int length) {
     lcd.setCursor(0, 1);
     lcd.print("                ");
     lcd.setCursor(0, 1);
-    lcd.print(0);
+    lcd.print(1);
     delay(1000);
   }
   else {
@@ -47,25 +55,26 @@ void callback(char* topic, byte * payload, unsigned int length) {
     lcd.setCursor(0, 1);
     lcd.print("                ");
     lcd.setCursor(0, 1);
-    lcd.print(1);
+    lcd.print(0);
     delay(1000);
   }
 }
 
 void setup() {
+  pinMode(ledVerde, OUTPUT);
   Serial.begin(9600);
   Serial.println("iniciando...");
   Ethernet.begin(mac);
   delay(1500);
   lastReconnectAttempt = 0;
 
-if (client.connect("arduino", "arduino", "arduino"))
+  if (client.connect("arduino", "arduino", "arduino"))
   {
-  // Conecta no topic para receber mensagens
-  client.subscribe("teste");
-  
-  Serial.println("Conectado MQTT");
-  delay(50);
+    // Conecta no topic para receber mensagens
+    client.subscribe("teste#");
+    digitalWrite(ledVerde, HIGH);
+    Serial.println("Conectado MQTT");
+    delay(50);
   }
 
   lcd.begin(16, 2);
@@ -75,6 +84,7 @@ if (client.connect("arduino", "arduino", "arduino"))
 void loop() {
 
   if (!client.connected()) {
+    digitalWrite(ledVerde, LOW);
     long now = millis();
     if ((now - lastReconnectAttempt) > 5000) {
       Serial.println("reconectando...");
